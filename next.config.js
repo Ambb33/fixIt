a88096ae62793fa0f5bf4242c6f49261fe84b3ef
Ambb33 <143352441+Ambb33@users.noreply.github.com>
2025-02-28 08:12:31 +0100

    case sentivitiy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    output: "standalone", // Enables Next.js standalone mode (better for hosting)
+    reactStrictMode: true, 
+    trailingSlash: true, // Ensures proper routing with static files
+    compress: true, // Enables gzip compression
+    poweredByHeader: false, // Removes 'X-Powered-By: Next.js' header for security
+  
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+      };
+      config.resolve.symlinks = false;
+      config.resolve.aliasFields = ["browser"];
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;  
