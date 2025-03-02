da8bc523819318bb9ae629f1dc0c9c207791570a
Ambb33 <143352441+Ambb33@users.noreply.github.com>
2025-03-02 19:12:04 +0100

    env

--- server.mjs
+++ server.mjs
@@ -1,8 +1,11 @@
-import { createServer } from 'http';
 import { parse } from 'url';
 import next from 'next';
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
