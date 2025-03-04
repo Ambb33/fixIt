8bb54a45dc1d85efe4897935697991f4b2c148f4
Andreea Mocan <andreeamocan89@gmail.com>
2025-03-04 09:04:53 +0100

    script update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,10 @@
 "use client";
-import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-      <Link href="/">Go back to Home</Link>
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
     </div>
   );
 }
