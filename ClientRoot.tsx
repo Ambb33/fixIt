0514ddde6203e74a4b2da36d24cf79aff3dcbd0f
Andreea Mocan <andreeamocan89@gmail.com>
2025-03-04 21:12:57 +0100

    fix

--- ClientRoot.tsx
+++ ClientRoot.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, ReactNode } from "react";
+import { ReactNode } from "react";
 import { Navbar, Footer } from "@components";
 import styles from "@styles/style";
 
@@ -9,38 +9,11 @@ interface ClientRootProps {
 }
 
 const ClientRoot = ({ children }: ClientRootProps) => {
-  const [error, setError] = useState<Error | null>(null);
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    // This will only run on the client-side
-    setIsClient(true);
-  }, []);
-
-  const reset = () => {
-    setError(null);
-    console.log("Reset function triggered");
-  };
-
-  if (!isClient) {
-    return null; // Or render a fallback until the component is client-side
-  }
-
   return (
     <div className="bg-primary w-full overflow-hidden">
       <Navbar />
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          {error ? (
-            <div>
-              <h2>Something went wrong!</h2>
-              <p>{error.message}</p>
-              <button onClick={reset}>Try again</button>
-            </div>
-          ) : (
-            children || "No content available"
-          )}
-        </div>
+        <div className={`${styles.boxWidth}`}>{children}</div>
       </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
