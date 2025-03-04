e06c5d75dbbb8eed673c96a3a119c215107c0474
Ambb33 <143352441+Ambb33@users.noreply.github.com>
2025-03-04 17:56:10 +0100

    iss node settings

--- server.mjs
+++ server.mjs
@@ -1,39 +1,31 @@
-import { parse } from 'url';
-import next from 'next';
-import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import dotenv from 'dotenv';
+import('express').then(({ default: express }) => {
+  import('dotenv').then(({ default: dotenv }) => {
+    dotenv.config();
+    const app = express();
+    const port = process.env.PORT || 3000;
+    
+    import('next').then(({ default: next }) => {
+      const dev = process.env.NODE_ENV !== 'production';
+      const nextApp = next({ dev });
+      const handle = nextApp.getRequestHandler();
 
-dotenv.config();
+      nextApp.prepare().then(() => {
+        const server = express();
 
+        import('http-proxy-middleware').then(({ createProxyMiddleware }) => {
+          server.use('/api', createProxyMiddleware({
+            target: 'http://fix.ambeautyboutique.com',
+            changeOrigin: true,
+            logLevel: 'debug'
+          }));
 
-const port = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+          server.all('*', (req, res) => handle(req, res));
 
-app.prepare().then(() => {
-  const server = express();
-
-  // Proxy API requests
-  server.use('/api', createProxyMiddleware({
-    target: 'http://fix.ambeautyboutique.com',
-    changeOrigin: true,
-    logLevel: 'debug'  // Add this line to enable logging for the proxy
-  }));
-
-  server.all('*', (req, res) => {
-    const parsedUrl = parse(req.url, true);
-    console.log(`Request received: ${req.url}`);
-    handle(req, res, parsedUrl);
-  });
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(
-      `> Server listening at http://localhost:${port} as ${
-        dev ? 'development' : process.env.NODE_ENV
-      }`
-    );
+          server.listen(port, () => {
+            console.log(`> Server running at http://localhost:${port}`);
+          });
+        });
+      });
+    });
   });
 });
