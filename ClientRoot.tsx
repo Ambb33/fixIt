da9d47f63efb1eac0b217a786cf0b5b3f93e3f42
Ambb33 <143352441+Ambb33@users.noreply.github.com>
2025-03-04 19:15:29 +0100

    client update

--- ClientRoot.tsx
+++ ClientRoot.tsx
@@ -1,46 +1,44 @@
 "use client";
 
-import { useState } from "react";
 import { Navbar, Footer } from "@components";
 import styles from "@styles/style";
 import { ReactNode } from "react";
 
+// Ensure the ClientRootProps interface is correctly defined here
 interface ClientRootProps {
   children: ReactNode;
 }
 
 const ClientRoot = ({ children }: ClientRootProps) => {
-  const [error, setError] = useState<Error | null>(null);
+  // const [error, setError] = useState<Error | null>(null);
 
-  const reset = () => {
-    setError(null);
-    console.log("Reset function triggered");
-  };
+  // const reset = () => {
+  //   setError(null);
+  //   console.log("Reset function triggered");
+  // };
 
   return (
-    <>
-      <div className="bg-primary w-full overflow-hidden">
-        <Navbar />
-        <div className={`bg-primary ${styles.flexStart}`}>
-          <div className={`${styles.boxWidth}`}>
-            {error ? (
-              <div>
-                <h2>Something went wrong!</h2>
-                <p>{error.message}</p>
-                <button onClick={reset}>Try again</button>
-              </div>
-            ) : (
-              children || "No content available"
-            )}
-          </div>
-        </div>
-        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-          <div className={`${styles.boxWidth}`}>
-            <Footer />
-          </div>
+    <div className="bg-primary w-full overflow-hidden">
+      <Navbar />
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          {/* {error ? (
+            <div>
+              <h2>Something went wrong!</h2>
+              <p>{error.message}</p>
+              <button onClick={reset}>Try again</button>
+            </div>
+          ) : ( */}
+            {children || "No content available"}
+          {/* )} */}
         </div>
       </div>
-    </>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Footer />
+        </div>
+      </div>
+    </div>
   );
 };
 
