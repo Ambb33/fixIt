96516b48a942bcf4beaf27b49e4d7613bfb3e9f0
Ambb33 <143352441+Ambb33@users.noreply.github.com>
2025-03-04 19:37:42 +0100

    ClientRoot

--- ClientRoot.tsx
+++ ClientRoot.tsx
@@ -1,36 +1,45 @@
 "use client";
 
+import { useState, useEffect, ReactNode } from "react";
 import { Navbar, Footer } from "@components";
 import styles from "@styles/style";
-import { ReactNode } from "react";
 
-// Ensure the ClientRootProps interface is correctly defined here
 interface ClientRootProps {
   children: ReactNode;
 }
 
 const ClientRoot = ({ children }: ClientRootProps) => {
-  // const [error, setError] = useState<Error | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [isClient, setIsClient] = useState(false);
 
-  // const reset = () => {
-  //   setError(null);
-  //   console.log("Reset function triggered");
-  // };
+  useEffect(() => {
+    // This will only run on the client-side
+    setIsClient(true);
+  }, []);
+
+  const reset = () => {
+    setError(null);
+    console.log("Reset function triggered");
+  };
+
+  if (!isClient) {
+    return null; // Or render a fallback until the component is client-side
+  }
 
   return (
     <div className="bg-primary w-full overflow-hidden">
       <Navbar />
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          {/* {error ? (
+          {error ? (
             <div>
               <h2>Something went wrong!</h2>
               <p>{error.message}</p>
               <button onClick={reset}>Try again</button>
             </div>
-          ) : ( */}
-            {children || "No content available"}
-          {/* )} */}
+          ) : (
+            children || "No content available"
+          )}
         </div>
       </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
