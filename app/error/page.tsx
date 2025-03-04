de8dc994a449e85a576b01a0429b4f3d96013e81
Andreea Mocan <andreeamocan89@gmail.com>
2025-03-04 01:10:38 +0100

    error page

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,11 +1,16 @@
+// app/error/page.tsx
 "use client"; // Ensure it's client-side
 
+interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
 
-export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+export default function ErrorPage({ error, reset }: ErrorProps) {
   return (
     <div>
       <h1>Something went wrong!</h1>
-      <p>{error.message}</p>  {/* Display the error message */}
+      <p>{error.message}</p>
       <button onClick={() => reset()}>Try again</button>
     </div>
   );
